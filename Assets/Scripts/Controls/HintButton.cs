b894ec950b4cc873893c237aabda1778c8485d0b
Malyglut <malygluttwitch@gmail.com>
2022-02-11 01:41:17 +0100

    updated hint visuals

---
+++ Assets/Scripts/Controls/HintButton.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sufka.Controls
+{
+    public class HintButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+
+        [SerializeField]
+        private TextMeshProUGUI _availableHints;
+    }
+}
