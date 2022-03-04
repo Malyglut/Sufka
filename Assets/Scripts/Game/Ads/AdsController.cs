252e3dbab06f0a201a4cb5c5e8416a969523ac66
Malyglut <malygluttwitch@gmail.com>
2022-03-04 15:17:20 +0100

    reworked ads initialization

--- Assets/Scripts/Game/Ads/AdsController.cs
+++ Assets/Scripts/Game/Ads/AdsController.cs
@@ -8,11 +8,19 @@ namespace Sufka.Game.Ads
         private const string IOS_ID = "4608226";
         private const string HINTS_AD_ID = "Hints";
         private const string BONUS_POINTS_AD_ID = "Bonus_Points";
+        
+        private bool _initalized;
+        
         public string HintsAdId => HINTS_AD_ID;
         public string BonusPointsAdId => BONUS_POINTS_AD_ID;
 
         public void Initialize(IUnityAdsListener listener)
         {
+            if (_initalized)
+            {
+                return;
+            }
+            
             Advertisement.AddListener(listener);
 
 #if UNITY_ANDROID
@@ -20,6 +28,8 @@ namespace Sufka.Game.Ads
 #elif UNITY_IOS
             Advertisement.Initialize(IOS_ID);
 #endif
+
+            _initalized = true;
         }
 
         public void PlayHintAd()
